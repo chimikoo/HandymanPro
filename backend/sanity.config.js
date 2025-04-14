313b8a75efa8e0927f15425d1784be0105313a11
Per-Emil Johansson <chimikoo@gmail.com>
2025-04-14 14:36:00 +0200

    env backend fix

--- backend/sanity.config.js
+++ backend/sanity.config.js
@@ -1,14 +1,21 @@
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemaTypes'
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { visionTool } from '@sanity/vision'
+import { schemaTypes } from './schemaTypes'
+
+const projectId = process.env.SANITY_PROJECT_ID
+const dataset = process.env.SANITY_DATASET
+
+if (!projectId || !dataset) {
+  throw new Error("Missing SANITY_PROJECT_ID or SANITY_DATASET in environment variables")
+}
 
 export default defineConfig({
   name: 'default',
   title: 'handyman',
 
-  projectId: 'mn8iiw8u',
-  dataset: 'production',
+  projectId,
+  dataset,
 
   plugins: [structureTool(), visionTool()],
 
