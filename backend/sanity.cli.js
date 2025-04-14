20b51cd19a6ebdca2a85237af76b4ef739c30df3
Per-Emil Johansson <chimikoo@gmail.com>
2025-04-14 14:22:23 +0200

    env file fix

--- backend/sanity.cli.js
+++ backend/sanity.cli.js
@@ -1,13 +1,12 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 export default defineCliConfig({
   api: {
-    projectId: 'mn8iiw8u',
-    dataset: 'production'
+    projectId: process.env.SANITY_PROJECT_ID,
+    dataset: process.env.SANITY_DATASET,
   },
-  /**
-   * Enable auto-updates for studios.
-   * Learn more at https://www.sanity.io/docs/cli#auto-updates
-   */
   autoUpdates: true,
 })
--- client/app/lib/sanity.client.ts
+++ client/app/lib/sanity.client.ts
@@ -1,8 +1,8 @@
 import { createClient } from 'next-sanity'
 
 export const sanityClient = createClient({
-  projectId: 'mn8iiw8u',
-  dataset: 'production',        // or your dataset name
-  useCdn: false,                 // `true` = faster, `false` = fresh
-  apiVersion: '2023-10-01',     // use today's date or latest version
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  useCdn: false,
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION!,
 })
