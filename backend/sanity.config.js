8f283b2a7e06264331a0579684add267808c2821
Per-Emil Johansson <chimikoo@gmail.com>
2025-04-14 15:31:56 +0200

    fix variables

--- backend/sanity.config.js
+++ backend/sanity.config.js
@@ -3,11 +3,11 @@ import { structureTool } from 'sanity/structure'
 import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './schemaTypes'
 
-const projectId = process.env.SANITY_PROJECT_ID
-const dataset = process.env.SANITY_DATASET
+const projectId = process.env.SANITY_STUDIO_PROJECT_ID
+const dataset = process.env.SANITY_STUDIO_DATASET
 
 if (!projectId || !dataset) {
-  throw new Error("Missing SANITY_PROJECT_ID or SANITY_DATASET in environment variables")
+  throw new Error("Missing SANITY_STUDIO_PROJECT_ID or SANITY_STUDIO_DATASET in environment variables")
 }
 
 export default defineConfig({
